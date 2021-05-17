c290d9b80cd510678d27458a16b3f707f02348ad
Index-Zero <index.zero.00@gmail.com>
2021-05-17 10:37:55 +0200

    The tcp client

---
+++ Client.py
@@ -0,0 +1,26 @@
+import socket
+from threading import Thread
+
+SERVER_IP = socket.gethostname()
+SERVER_PORT = 1234
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, SERVER_PORT))
+
+
+def receivingMsg(conn):
+    while True:
+        msg = conn.recv(128).decode("utf-8")
+        if msg:
+            print(msg)
+
+
+recievThread = Thread(target=receivingMsg, args=(client,))
+recievThread.daemon = True
+recievThread.start()
+msg = client.recv(128).decode('utf-8')
+print(msg)
+
+while True:
+    msg = "Client@ "
+    msg += input("")
+    client.send(msg.encode('utf-8'))
